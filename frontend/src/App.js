65d19bc342c194b6f9e03ba1cb5230a3ee4bcc47
Joynal Abedin <cfcfahim26@gmail.com>
2023-05-13 23:16:20 +0600

    logout added with ui updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,12 +14,16 @@ import AuthenticationPage, { action as authAction } from './pages/Authentication
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import { action as logoutAction } from './pages/Logout'
+import { tokenLoader } from './utils/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -64,7 +68,11 @@ const router = createBrowserRouter([
         path: 'auth',
         element: <AuthenticationPage />,
         action: authAction
-      }
+      },
+      {
+        path: 'logout',
+        action: logoutAction
+      },
     ],
   },
 ]);
