a7d93f433a0da946d201db1deb1837cda04eee90
Joynal Abedin <cfcfahim26@gmail.com>
2023-05-14 21:52:52 +0600

    token expiration added

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,27 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../utils/auth';
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'POST' });
+    }
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'POST' });
+    }, tokenDuration);
+  }, [token, submit])
 
   return (
     <>
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,8 +1,42 @@
+import { redirect } from "react-router-dom";
+
+export const getTokenDuration = () => {
+  const storedExpirationDate = localStorage.getItem('expiration');
+  const expirationDate = new Date(storedExpirationDate);
+
+  const currentDate = new Date();
+
+  const remainingDuration = expirationDate.getTime() - currentDate.getTime();
+
+  return remainingDuration;
+}
+
 export const getAuthToken = () => {
   const token = localStorage.getItem('token')
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return 'EXPIRED';
+  }
+
   return token;
 }
 
 export const tokenLoader = () => {
   return getAuthToken();
+}
+
+export const checkAuthLoader = () => {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect('/auth');
+  }
+
+  return null;
 }
